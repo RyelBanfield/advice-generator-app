1f73dd64553be72c105402a950210a339b7d9cf3
Ryel Banfield <ryelbanfield@gmail.com>
2023-04-20 12:01:42 -0400

    Use router.refresh instead of window.reload

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Button = () => {
+  const router = useRouter();
+
   return (
     <button
       onClick={() => {
-        window.location.reload();
+        router.refresh();
       }}
-      className="bg-primary-NeonGreen rounded-full w-16 h-16 flex items-center justify-center absolute bottom-[-32px] m-auto left-0 right-0 transition ease-in-out hover:shadow-primary-NeonGreen hover:shadow-[0_0_20px_0] transform hover:scale-110 duration-300"
+      className="absolute bottom-[-32px] left-0 right-0 m-auto flex h-16 w-16 transform items-center justify-center rounded-full bg-primary-NeonGreen transition duration-300 ease-in-out hover:scale-110 hover:shadow-[0_0_20px_0] hover:shadow-primary-NeonGreen"
     >
       <Image src="icon-dice.svg" alt="Dice" width={25} height={25} />
     </button>
