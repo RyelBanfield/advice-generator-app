f8df14f3797c17b7377e46016ef05420fd8f525e
Ryel Banfield <ryelbanfield@gmail.com>
2023-04-21 11:55:51 -0400

    Animate button with Framer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -7,14 +8,16 @@ const Button = () => {
   const router = useRouter();
 
   return (
-    <button
+    <motion.button
       onClick={() => {
         router.refresh();
       }}
-      className="absolute bottom-[-32px] left-0 right-0 m-auto flex h-16 w-16 transform items-center justify-center rounded-full bg-primary-NeonGreen transition duration-300 ease-in-out hover:scale-110 hover:shadow-[0_0_20px_0] hover:shadow-primary-NeonGreen"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9, rotate: 180 }}
+      className="absolute bottom-[-32px] left-0 right-0 m-auto flex h-16 w-16 transform items-center justify-center rounded-full bg-primary-NeonGreen transition duration-75 ease-in-out hover:shadow-[0_0_20px_0] hover:shadow-primary-NeonGreen"
     >
       <Image src="icon-dice.svg" alt="Dice" width={25} height={25} />
-    </button>
+    </motion.button>
   );
 };
 
